a3d94597fb8b1cf227bb9428ba8636043e3c5322
Zy4-Ha8 <haiderumbra@gmail.com>
2025-04-11 14:44:16 +0300

    solve issue

--- src/components/404page/NotFound404.jsx
+++ src/components/404page/NotFound404.jsx
@@ -3,7 +3,7 @@ import "./notFoundStyle.css";
 const NotFound404 = () => {
   return (
     <section className="page_404">
-      <div className="container h-screen">
+      <div className="container h-screen text-black">
         <div className="row">
           <div className="col-sm-12 ">
             <div className="col-sm-10 col-sm-offset-1  text-center">
